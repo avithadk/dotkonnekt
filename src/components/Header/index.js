ed925cd0af2a3bbcc76c77473ae3e6f757b384c9
deepakdotkonnekt <deepak.dasan@dotkonnekt.com>
2023-05-23 11:12:20 +0530

    test pr

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,11 +3,10 @@ import Logo from "./Logo";
 import ResponsiveAppBar from "./Header";
 const demoFn = () => {
   return (
-    <div>
-      HAI
+    <>
       <Logo />
       <ResponsiveAppBar />
-    </div>
+    </>
   );
 };
 
